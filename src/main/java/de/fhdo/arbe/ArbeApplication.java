f8c0a74e3b3bbf1fe6cc2ac7ffe50ef2fd0f3976
mikillin <sergey.rogachevsky@gmail.com>
2024-10-21 05:50:07 +0200

    Scratch project

---
+++ src/main/java/de/fhdo/arbe/ArbeApplication.java
@@ -0,0 +1,13 @@
+package de.fhdo.arbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/fhdo/arbe/ArbeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fhdo.arbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
